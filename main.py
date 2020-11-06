b6082a10a47c9c5f5982ea8f1e23fda496c96617
mogham <mohamed.khairallah.gharbi@gmail.com>
2020-11-06 16:03:36 +0100

    Adding simutation type input

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from MachineLine import Machine, Buffer, System, INPUT_BUF, OUTPUT_BUF
 
 
 def main():
-    print("Simulation")
-    print("Creating the System")
+    print("------Simulation------")
+    print("---Creating the System")
     numberMachine = int(input("Enter the number of machines: "))
     machine = []
     bufferTable = []
@@ -21,8 +21,15 @@ def main():
             machineTable.append(Machine(breakdown_prob,repair_prob,bufferTable[j-1],OUTPUT_BUF,"Machine"+str(j+1)))
         else:  
             machineTable.append(Machine(breakdown_prob,repair_prob,bufferTable[j-1],bufferTable[j],"Machine"+str(j+1)))
-    
     system = System(numberMachine, machineTable, bufferTable)
+    print("------Choosing simulation type------")
+    print("--Click A: Automatic simulation")
+    print("--Click B: 1-step simulation")
+    choice = input("Choose simulation type : ")
+    if (choice == "A"):
+        timeUnit = input("Enter the time slot : ")
+    
+
 
 if __name__ == "__main__":
     main()
